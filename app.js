151cebcde9e772578deb04142e6d9d31a1f66747
Gulyapas Poonkawinsiri <gulyapas.p@gmail.com>
2022-07-10 23:44:06 +0700

    using middleware

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const express = require("express");
 
 const app = express();
 
+// add middleware function
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next(); // allow the request to continue to the next middleware in line
+}); 
+
+// add another middleware function
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+  // ....
+}); 
+
+// when visiting localhost:3000, the following will be logged
+// In the middleware!
+// In another middleware!
+
 const server = http.createServer(app);
 
 server.listen(3000);
