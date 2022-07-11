5c9034f5a66e2aa3fb3ac43517c81b16de9bbb92
Gulyapas Poonkawinsiri <gulyapas.p@gmail.com>
2022-07-11 11:55:08 +0700

    using helper function for navigation, creating root directory for html path to be served

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const express = require("express");
 
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 // same as using app.use() app.get() app.post()
 // path is now /admin/add-product (GET)
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // path is now /admin/add-product (POST)
